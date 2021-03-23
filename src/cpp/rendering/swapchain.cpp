8abd0125ffbf49b3dcfa0d7618f8c636b4bf4d59
splendour <splendourbg@gmail.com>
2021-03-23 22:58:14 +0200

    Only compile the swapchain that is in use

--- src/cpp/rendering/swapchain.cpp
+++ src/cpp/rendering/swapchain.cpp
@@ -1,3 +1,9 @@
 #include "common.h"
 
 #include "swapchain.h"
+
+#if SWAPCHAIN_DXGI
+#include "platform/dxgi/swapchain_dxgi.hpp"
+#else
+#include "platform/vulkan/swapchain_vulkan.hpp"
+#endif
--- src/cpp/rendering/swapchain.h
+++ src/cpp/rendering/swapchain.h
@@ -1,7 +1,12 @@
 #pragma once
 
-//#ifdef _WIN32
-#if 0
+#ifdef _WIN32
+#define SWAPCHAIN_DXGI 0
+#else
+#define SWAPCHAIN_DXGI 0
+#endif
+
+#if SWAPCHAIN_DXGI
 #include "platform/dxgi/swapchain_dxgi.h"
 using Swapchain = Swapchain_DXGI;
 #else
